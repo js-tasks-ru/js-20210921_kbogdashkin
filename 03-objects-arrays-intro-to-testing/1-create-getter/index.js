848e69df85047668ced824340f4a3e89b97e1de4
kbogdashkin <bogdashkinkonstantin@yandex.ru>
2021-10-01 19:12:49 +0200

    fix to getter function

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,6 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const parts = path.split('.');
+
   return function (obj) {
     if (!path) {
       return obj;
